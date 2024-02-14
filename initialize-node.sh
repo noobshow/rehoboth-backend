e1b658da195e42b449cb410c718a18be5c058af1
Nikki Samson” <nikkisamsonme@gmail.com>
2024-02-14 17:02:05 +0100

    fixing pusher

--- initialize-node.sh
+++ initialize-node.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+# This script will initialize node.js environment for the user
+# Run this script only in user's home directory, not as root 
+# Check if the user is root
+if [ "$EUID" -eq 0 ]
+  then echo "Please do not run as root"
+  exit
+fi
 echo 'Setting up npm global packages path'
 echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile
 echo 'Adding ea-nodejs20 node binaries to PATH'
-echo 'export PATH=$PATH:/opt/cpanel/ea-nodejs20/bin' >> ~/.profile
+echo 'export PATH=$PATH:/opt/cpanel/ea-nodejs18/bin' >> ~/.profile
 echo 'Refreshing Current Shell Session'
 source ~/.profile
+# remove ea-nodejs20 from PATH
+sed -i '/ea-nodejs20/d' ~/.profile
+# remove npm-global from PATH
