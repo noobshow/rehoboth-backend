bd4ffc5ec40e9e2f841261485bbac1d65579fdb8
Nikki Samson” <nikkisamsonme@gmail.com>
2024-02-16 18:12:20 +0100

    removed all limitations from broadcaster

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,91 +21,53 @@ class Kernel extends ConsoleKernel
             $tradeBlotterSignals = \App\Models\TradeBlotter::all();
 
             // Log::info('OptimusSignalUpdated event firing');
-            $cdlSignalsTime = Settings::where('name', 'cdlSignals')->first();
-            $coreSignalsTime = Settings::where('name', 'coreSignals')->first();
-            $newsDataSignalsTime = Settings::where('name', 'newsDataSignals')->first();
-            $tradeBlotterSignalsTime = Settings::where('name', 'tradeBlotterSignals')->first();
+            // $cdlSignalsTime = Settings::where('name', 'cdlSignals')->first();
+            // $coreSignalsTime = Settings::where('name', 'coreSignals')->first();
+            // $newsDataSignalsTime = Settings::where('name', 'newsDataSignals')->first();
+            // $tradeBlotterSignalsTime = Settings::where('name', 'tradeBlotterSignals')->first();
 
-            if ($cdlSignalsTime == null) {
-                $cdlSignalsTime = new Settings();
-                $cdlSignalsTime->name = 'cdlSignals';
-                $cdlSignalsTime->value = now()->addMinutes(3);
-                $cdlSignalsTime->save();
-                // fire event
-                event(new \App\Events\CDLSignalUpdated(
-                    $cdlSignals->toArray()
-                ));
-                // Log::info('cdlSignals: ' . $cdlSignals->toJson());
-            } else if ($cdlSignalsTime->value < now()) {
-                $cdlSignalsTime->value = now()->addMinutes(3);
-                $cdlSignalsTime->save();
-                // fire event
-                // Log::info('cdlSignals: ' . $cdlSignals->toJson());
-                event(new \App\Events\CDLSignalUpdated(
-                    $cdlSignals->toArray()
-                ));
-            }
+            // if ($cdlSignalsTime == null) {
+            //     $cdlSignalsTime = new Settings();
+            //     $cdlSignalsTime->name = 'cdlSignals';
+            //     $cdlSignalsTime->value = now()->addMinutes(3);
+            //     $cdlSignalsTime->save();
+            // fire event
+            event(new \App\Events\CDLSignalUpdated(
+                $cdlSignals->toArray()
+            ));
 
             event(new \App\Events\OptimusSignalUpdated(
-                    $coreSignals->toArray()
-                ));
+                $coreSignals->toArray()
+            ));
 
-            if ($newsDataSignalsTime == null) {
-                $newsDataSignalsTime = new Settings();
-                $newsDataSignalsTime->name = 'newsDataSignals';
-                $newsDataSignalsTime->value = now()->addMinutes(10);
-                $newsDataSignalsTime->save();
-
-                $result = [];
-                $signals = $newsDataSignals->take(5);
-                foreach ($signals as $item) {
-                    $tmp = new \stdClass();
-                    $tmp->time = $item->time->format('H:i');
-                    $tmp->event = trim($item->event);
-                    $tmp->impact = $item->impact;
-                    $tmp->actual = $item->actual;
-                    $tmp->forecast = $item->forecast;
-                    $tmp->previous = $item->previous;
-                    $result[] = $tmp;
-                }
-                // fire event
-                event(new \App\Events\NewsDataUpdated(
-                    $result
-                ));
-                // Log::info('newsDataSignals: ' . $newsDataSignals->toJson());
-            } else if ($newsDataSignalsTime->value < now()) {
-                $newsDataSignalsTime->value = now()->addMinutes(3);
-                $newsDataSignalsTime->save();
-
-                $result = [];
-                $signals = $newsDataSignals->take(5);
-                foreach ($signals as $item) {
-                    $tmp = new \stdClass();
-                    $tmp->time = $item->time->format('H:i');
-                    $tmp->event = trim($item->event);
-                    $tmp->impact = $item->impact;
-                    $tmp->actual = $item->actual;
-                    $tmp->forecast = $item->forecast;
-                    $tmp->previous = $item->previous;
-                    $result[] = $tmp;
-                }
-
-                // Log::info('newsDataSignals: ' . $newsDataSignals->toJson());
-                event(new \App\Events\NewsDataUpdated(
-                    $result
-                ));
+            $result = [];
+            $signals = $newsDataSignals->take(5);
+            foreach ($signals as $item) {
+                $tmp = new \stdClass();
+                $tmp->time = $item->time->format('H:i');
+                $tmp->event = trim($item->event);
+                $tmp->impact = $item->impact;
+                $tmp->actual = $item->actual;
+                $tmp->forecast = $item->forecast;
+                $tmp->previous = $item->previous;
+                $result[] = $tmp;
             }
+            // fire event
+            event(new \App\Events\NewsDataUpdated(
+                $result
+            ));
+            // Log::info('newsDataSignals: ' . $newsDataSignals->toJson());
 
             // if ($tradeBlotterSignalsTime == null) {
             //     $tradeBlotterSignalsTime = new Settings();
             //     $tradeBlotterSignalsTime->name = 'tradeBlotterSignals';
             //     $tradeBlotterSignalsTime->value = now()->addMinutes(1);
             //     $tradeBlotterSignalsTime->save();
-                // fire event
-                event(new \App\Events\TradeBlotterUpdated(
-                    $tradeBlotterSignals->toArray()
-                ));
-                // Log::info('tradeBlotterSignals: ' . $tradeBlotterSignals->toJson());
+            // fire event
+            event(new \App\Events\TradeBlotterUpdated(
+                $tradeBlotterSignals->toArray()
+            ));
+            // Log::info('tradeBlotterSignals: ' . $tradeBlotterSignals->toJson());
             // } else if ($tradeBlotterSignalsTime->value < now()) {
             //     $tradeBlotterSignalsTime->value = now()->addMinutes(1);
             //     $tradeBlotterSignalsTime->save();
