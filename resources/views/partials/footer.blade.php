23c4c7c750fa2fc97753682e59d42b8e7976e574
Nikki Samson” <nikkisamsonme@gmail.com>
2024-02-27 00:05:15 +0100

    fixed risk  and disclaimer padding

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -19,7 +19,7 @@
 	</div>
 	<!--end::Container-->
 	<!--begin::Container-->
-	<div class="container-xxl d-flex flex-column flex-md-row align-items-center justify-content-between">
+	<div class="container-xxl pt-5 d-flex flex-column flex-md-row align-items-center justify-content-between">
 		<!--begin::Copyright-->
 		<div class="text-dark order-2 order-md-1">
 			<span class="fw-bold me-1">Risk Warning and Disclaimer</span>
@@ -28,7 +28,7 @@
 	</div>
 	<!--end::Container-->
 	<!--begin::Container-->
-	<div class="container-xxl pt-5 d-flex flex-column flex-md-row align-items-center justify-content-between">
+	<div class="container-xxl d-flex flex-column flex-md-row align-items-center justify-content-between">
 		<!--begin::Disclaimer-->
 		<div class="text-dark order-2 order-md-1">
 			<span class="text-muted fw-bold me-1">Trading on financial markets come with a high risk of losing fund swiftly due to considerable risk exposure or leverage. History shows that about majority of retail investor accounts lose fund when Trading on financial markets with their providers. You should therefore, consider whether you understand how financial markets work and whether you can afford to take the high risk of losing your fund. To be informed, you can lose all, but not more than the balance of your Trading Account. These products may not be suitable for all clients therefore ensure you understand the risks and seek independent financial adviser. Rehoboth Traders LTD accepts no responsibility for any use of her instruments and for any outcome that result. No representation or warranty is given as to the absolute or correctness of this information, therefore, any person acting on it does so entirely at their own risk.</span>
