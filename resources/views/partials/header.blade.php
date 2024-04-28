e712199ca9ec8bf1fe954ec8606170bdd557102b
Nikki Samson” <nikkisamsonme@gmail.com>
2024-04-28 21:48:58 +0100

    fix chat icon remove number

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -29,7 +29,15 @@
                 <!--begin::Chat-->
                 <div class="d-flex align-items-center ms-3 ms-lg-4">
                     <!--begin::Drawer wrapper-->
-                    <div class="btn btn-icon btn-primary position-relative w-30px h-30px w-lg-40px h-lg-40px" id="kt_drawer_chat_toggle">{{count($messages)}}
+                    <!-- <div class="btn btn-icon btn-primary position-relative w-30px h-30px w-lg-40px h-lg-40px" id="kt_drawer_chat_toggle">{{count($messages)}} -->
+                    <div class="btn btn-icon btn-dark btn-active-color-primary btn-outline btn-outline-secondary w-30px h-30px w-lg-40px h-lg-40px" id="kt_drawer_chat_toggle">
+                        <!--begin::Svg Icon | path: assets/media/icons/duotune/communication/com007.svg-->
+                        <span class="svg-icon svg-icon-muted svg-icon-2hx"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path opacity="0.3" d="M8 8C8 7.4 8.4 7 9 7H16V3C16 2.4 15.6 2 15 2H3C2.4 2 2 2.4 2 3V13C2 13.6 2.4 14 3 14H5V16.1C5 16.8 5.79999 17.1 6.29999 16.6L8 14.9V8Z" fill="black"></path>
+                            <path d="M22 8V18C22 18.6 21.6 19 21 19H19V21.1C19 21.8 18.2 22.1 17.7 21.6L15 18.9H9C8.4 18.9 8 18.5 8 17.9V7.90002C8 7.30002 8.4 6.90002 9 6.90002H21C21.6 7.00002 22 7.4 22 8ZM19 11C19 10.4 18.6 10 18 10H12C11.4 10 11 10.4 11 11C11 11.6 11.4 12 12 12H18C18.6 12 19 11.6 19 11ZM17 15C17 14.4 16.6 14 16 14H12C11.4 14 11 14.4 11 15C11 15.6 11.4 16 12 16H16C16.6 16 17 15.6 17 15Z" fill="black"></path>
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->
                         <!--begin::Bullet-->
                         <span class="d-none bullet bullet-dot bg-danger h-6px w-6px position-absolute translate-middle top-0 start-50 animation-blink"></span>
                         <!--end::Bullet-->
